d4377afaa22f0c96b04489bb6cc1688d9356a623
Klhnikov Riton <parisot.eric.mail@gmail.com>
2018-08-04 03:03:57 +0200

    Signed-off-by: Klhnikov Riton <parisot.eric.mail@gmail.com>

--- ImagesClassifier/model/urls.py
+++ ImagesClassifier/model/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='model'),
-    path('in_layer', views.in_layer, name='in_layer'),
-    path('dense_layer', views.dense_layer, name='dense_layer'),
-    path('conv2d_layer', views.conv2d_layer, name='conv2d_layer'),
-    path('dropout_layer', views.dropout_layer, name='dropout_layer'),
-    path('max_pool_layer', views.max_pool_layer, name='max_pool_layer'),
+    path('set_layer', views.set_layer, name='set_layer'),
 ]
