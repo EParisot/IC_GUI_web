2591930ab1fd92d2f278394018268b760dea6669
Klhnikov Riton <parisot.eric.mail@gmail.com>
2018-08-04 11:59:13 +0200

    Signed-off-by: Klhnikov Riton <parisot.eric.mail@gmail.com>

--- ImagesClassifier/model/views.py
+++ ImagesClassifier/model/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     return render(request, 'model/model.html')
 
+@login_required
 def set_layer(request):
-    yield render(request, 'model/set_layer.html')
+    return render(request, 'model/set_layer.html')
